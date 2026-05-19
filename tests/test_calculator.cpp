7ac10c12739f7af4e45ba657655f160bb1a4da89
liuwenchen1 <liuwenchen1@lixiang.com>
2026-05-19 17:24:30 +0800

    chore: organize source and test files

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -1,7 +1,6 @@
 #include "gtest/gtest.h"
 #include "calculator.h"
 
-// 测试计算器的多个操作
 class CalculatorTest : public ::testing::TestWithParam<
     std::tuple<double, double, char, double>> {
 };
@@ -36,14 +35,39 @@ INSTANTIATE_TEST_SUITE_P(
     )
 );
 
-// 使用 Combine 生成笛卡尔积
+class CalculatorCombinedTest : public ::testing::TestWithParam<
+    std::tuple<double, double, char>> {
+};
+
+TEST_P(CalculatorCombinedTest, Operations) {
+    double a = std::get<0>(GetParam());
+    double b = std::get<1>(GetParam());
+    char op = std::get<2>(GetParam());
+
+    Calculator calc;
+    double result = 0.0;
+    double expected = 0.0;
+
+    switch (op) {
+        case '+':
+            result = calc.add(a, b);
+            expected = a + b;
+            break;
+        case '-':
+            result = calc.subtract(a, b);
+            expected = a - b;
+            break;
+    }
+
+    EXPECT_NEAR(result, expected, 0.0001);
+}
+
 INSTANTIATE_TEST_SUITE_P(
     CombinedOperations,
-    CalculatorTest,
+    CalculatorCombinedTest,
     ::testing::Combine(
         ::testing::Values(1.0, 2.0),      // a
         ::testing::Values(3.0, 4.0),      // b
-        ::testing::Values('+', '-'),      // op
-        ::testing::Values(4.0, 5.0, -2.0, -3.0)  // expected (需要匹配)
+        ::testing::Values('+', '-')       // op
     )
 );
